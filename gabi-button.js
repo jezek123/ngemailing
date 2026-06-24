b827a3da074b98b87aa6043eafce3141aed27450
jezek123 <jezek@ngstranky.cz>
2026-06-24 12:16:58 +0200

    Create gabi-button.js

---
+++ gabi-button.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var btn = document.querySelector('input[type="submit"][name="yt0"]');
+
+    if (btn) {
+        btn.value = 'Chci být mezi prvními';
+    }
+});
