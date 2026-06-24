373d37b961c0830a559ef43bddea61de0155ac9b
jezek123 <jezek@ngstranky.cz>
2026-06-24 12:42:16 +0200

    Update gabi-button.js

--- gabi-button.js
+++ gabi-button.js
@@ -1,12 +1,5 @@
-console.log('GABI BUTTON LOADED');
+var btn = document.querySelector('input[type="submit"][name="yt0"]');
 
-setTimeout(function() {
-    var btn = document.querySelector('input[type="submit"][name="yt0"]');
-
-    if (btn) {
-        btn.value = 'Chci být mezi prvními';
-        console.log('BUTTON FOUND');
-    } else {
-        console.log('BUTTON NOT FOUND');
-    }
-}, 1000);
+if (btn) {
+    btn.value = 'Chci být mezi prvními';
+}
